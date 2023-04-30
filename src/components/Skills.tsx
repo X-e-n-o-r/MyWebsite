b70d7cf1ef1675991537c8ba9991975fdf3987a6
Xenor <shokot_2005@mail.ru>
2023-04-30 23:50:11 +0300

    q

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,22 +6,19 @@ export default function Skills() {
   return (
     <div className="skills">
       <section className="text-zone">
-        <header>
-          <h2>
-            <span className="h2-head">M</span>
-            <span className="h2-head">y</span>
-            <span className="h2-head"> s</span>
-            <span className="h2-head">k</span>
-            <span className="h2-head"></span>
-            <span className="h2-head">i</span>
-            <span className="h2-head">l</span>
-            <span className="h2-head">l</span>
-            <span className="h2-head">s</span>
-          </h2>
-        </header>
-        <p>1.0 and HTsionate about web.</p>
-        <p>killa whargtftgrtgr</p>
-        <p>trggtrtgrtrg</p>
+        <h2 className="skills-header">
+          <span className="h2-head">M</span>
+          <span className="h2-head">y</span>
+          <span className="h2-head"> s</span>
+          <span className="h2-head">k</span>
+          <span className="h2-head"></span>
+          <span className="h2-head">i</span>
+          <span className="h2-head">l</span>
+          <span className="h2-head">l</span>
+          <span className="h2-head">s</span>
+        </h2>
+        <p>1.0 and HTsionate about web.1.0 and HTsionate about web<br/>
+        1.0 and HTsionate about web</p>
       </section>
     </div>
   )
