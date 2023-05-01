6207cf1740e4221554bb47e9d3ab8ba1b8001000
Xenor <shokot_2005@mail.ru>
2023-05-01 16:30:57 +0300

    adaptivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import ContactMe from "./components/ContactMe"
-import Header from "./components/Header"
-import Projects from "./components/Projects"
+
 import Skills from "./components/Skills"
 
 
