202949f4d551045abca444091c497b60e9fd9d61
Xenor <shokot_2005@mail.ru>
2023-05-01 20:49:56 +0300

    tagcloud test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import Header from "./components/Header"
-import Skills from "./components/Skills"
-import '../src/Styles/TextShpere.css'
-function App() {
+import { TagCloud, TagCloudOptions } from "@frank-mayer/react-tag-cloud";
+// same as: import TagCloud from "@frank-mayer/react-tag-cloud"
 
-  return (
-    <>
-      <Header />
-      <Skills />
-    </>
-  )
-}
+const App = () => (
+    <TagCloud
+        options={(w: Window & typeof globalThis): TagCloudOptions => ({
+            radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
+            maxSpeed: "fast",
+        })}
+        onClick={(tag: string, _ev: MouseEvent) => alert(tag)}
+        onClickOptions={{ passive: true }}
+    >
+        {[
+            "VSCode",
+            "TypeScript",
+            "React",
+            "Preact",
+            "Parcel",
+            "Jest",
+            "Next",
+            "ESLint",
+            "Framer Motion",
+            "Three.js",
+        ]}
+    </TagCloud>
+);
 
 export default App
